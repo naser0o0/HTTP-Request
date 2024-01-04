e9a2e888350369b8c9a12ee461e65da490e3cdea
Naser <108549635+naser0o0@users.noreply.github.com>
2024-01-04 15:01:05 +0100

    put-data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -9,6 +9,31 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [error, setError] = useState();
 
+
+  // useEffect(() => {
+  //   async function fetchPlaces() {
+  //     setIsFetching(true);
+  //     try {
+  //       const response = await fetch("http://localhost:3000/places");
+  //       const resData = await response.json();
+
+  //       if (!response.ok) {
+  //         throw new Error("Failed to fetch places");
+  //       }
+
+  //       setAvailablePlaces(resData.places);
+  //     } catch (error) {
+  //       setError({
+  //         message:
+  //           error.message || "Could not fetch places, please try again later.",
+  //       });
+  //     }
+  //     setIsFetching(false);
+  //   }
+
+  //   fetchPlaces();
+  // }, []);
+
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
--- src/components/http.js
+++ src/components/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces(){
 
      return resData.places;
 }
+
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    // Body einer HTTP-Anfrage: Der Body einer HTTP-Anfrage enthält normalerweise die Daten, die an den Server gesendet werden. Bei vielen Anfragen wird der Body in Form von JSON übermittelt. Die JSON.stringify()-Methode wird verwendet, um sicherzustellen, dass die Daten im richtigen Format (JSON) vorliegen.
+    body: JSON.stringify({places}),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  // Nach dem Senden der Anfrage wartet die Funktion darauf, dass die Antwort des Servers als JSON-Objekt geparsed wird. Dies geschieht asynchron, daher wird await verwendet.
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to update user data. ");
+  }
+  return resData.message;
+}
